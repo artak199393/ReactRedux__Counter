c0064afe2dd56d869cb27ff1343c97b0eda6e77a
Artak <artak199393@gmail.com>
2021-03-21 15:49:44 +0400

    counter has  working in react redux

--- src/index.js
+++ src/index.js
@@ -1,39 +1,20 @@
-
+import React from 'react';
+import ReactDom from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
-const counter  = (state = 0, action) =>{
-    switch(action.type){
-        case 'INC':
-            return state +1;
-        case 'DEC':
-            return state -1;
-        case 'RES':
-            return 0;
-        default:
-            return state;
-    }
-}
 
-const store = createStore(counter);
+const store = createStore(reducer);
 
-const inc = () => ( {type: 'INC'} );
-const dec = () => ( {type: 'DEC'} );
-const res = () => ( {type: 'RES'} );
-setInterval(()=>{store.dispatch(inc())},1000)
-// document.getElementById('inc').addEventListener('click', () =>{
-//     store.dispatch(inc());
-// })
-document.getElementById('dec').addEventListener('click', () =>{
-    store.dispatch(dec());
-})
-document.getElementById('res').addEventListener('click', () =>{
-    store.dispatch(res());
-})
-const update=() =>{
-    document.getElementById('counter').textContent = store.getState();
-}
-store.subscribe(update);
 
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
+// const {inc, dec, res} =  bindActionCreators(actions, dispatch);
+
+    ReactDom.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
+
+
+
